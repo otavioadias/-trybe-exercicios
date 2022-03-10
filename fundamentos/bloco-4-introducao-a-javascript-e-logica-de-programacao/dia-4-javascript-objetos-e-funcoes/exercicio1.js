fbffdd05c73511b97427b9820265814645e9db59
Otávio Azevedo <otavioad1997@gmail.com>
2022-03-10 18:26:19 -0300

    Adiciona tarefa 5.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
@@ -16,3 +16,14 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+};
+
+for (let key in info) {
+  console.log(info[key] + ", " + info2[key]);
+}
