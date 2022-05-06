f8f177de288df52cabf950fbe4e8da3b23af3cf5
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-06 15:46:02 -0300

    Exercício 2

---
+++ front-end/bloco-10-introducao-a-react/header-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Content from './Content';
+
+import Header from './Header';
+
+import Footer from './Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
