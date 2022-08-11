ea83519d13b8a5629a2d6da59b9edf5f0fd507ea
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-11 18:27:55 -0300

    Exercício 7

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/ex7.sql
@@ -0,0 +1,6 @@
+-- Active: 1659640320529@@127.0.0.1@3306@hr
+# 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão (job_id).
+SELECT job_id, SUM(salary) FROM hr.employees
+GROUP BY  job_id;
+
+
