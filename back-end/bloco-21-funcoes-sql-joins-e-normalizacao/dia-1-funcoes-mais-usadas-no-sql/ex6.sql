8b07f528011fd216e0a7a980cc61b2477bfc89bc
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-11 18:26:12 -0300

    Exercício 6

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/ex6.sql
@@ -0,0 +1,7 @@
+-- Active: 1659640320529@@127.0.0.1@3306@hr
+# 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras (it_prog).
+SELECT COUNT(job_id) FROM hr.employees
+GROUP BY  job_id
+HAVING job_id = 'IT_PROG';
+
+
